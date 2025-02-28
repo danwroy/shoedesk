6a4b38b37daca729b55e1180d2705af07c28f09b
danwroy <dawroy@gmail.com>
2025-02-28 14:53:50 -0800

    "Objects" update

--- method/objects.go
+++ method/objects.go
@@ -33,6 +33,18 @@ func (sx Sex) String() string {
     return [...]{"men's", "women's"}[sx]
 }
 
+// return Sex value for string
+func SexToInt(sx string) (int, error){
+
+    lookup := map[string]int{"men's":0, "women's":1}
+
+    sex, ok := lookup[sx]
+    if !ok {
+        return fmt.Errorf("%s is incorrect, \"men's\" or \"women's\" only please", sx)
+    }
+    return sex
+}
+
 // return string for Request value
 func (rq Request) String() string (
     return [...]string{"borrow", "return"}[rq]
@@ -41,7 +53,7 @@ func (rq Request) String() string (
 var (
     ShoeReturn  map[Shoes]int = make(map[Shoes]int)                                             // Create dict representing shoe return
     Limits      map[string]Limit = make(map[string]Limit)                                       // Defined @ settings.go
-    // SexStr      map[boolean]string = map[boolean]string{true: "men's", false: "women's"})   // Map boolean to sex
-    // RequestStr  map[boolean]string = map[boolean]string{true: "borrow", false:"return"})    // Map boolean to request type
+    // SexStr      map[boolean]string = map[boolean]string{true: "men's", false: "women's"})       // Map boolean to sex
+    // RequestStr  map[boolean]string = map[boolean]string{true: "borrow", false:"return"})        // Map boolean to request type
 )
 
